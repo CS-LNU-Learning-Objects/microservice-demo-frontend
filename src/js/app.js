e57adfab09945a9abb3191fb7845bac4409ec263
John Häggerud <john.haggerud@lnu.se>
2019-05-16 10:37:46 +0200

    updating the UI and logger

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,10 @@ logger.write('Init the logger...')
 
 let login = async (url, email, password) => {
   logger.write('user try to login....')
-  let result
+  logger.line()
+
   try {
-    result = await window.fetch(url, {
+    let response = await window.fetch(url, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -16,18 +17,39 @@ let login = async (url, email, password) => {
       },
       body: JSON.stringify({ email: email, password: password })
     })
+    console.log(response)
+    logger.write('HTTP status: ' + response.status)
+    let result = await response.json()
+    logger.write('Issuer: ' + result.issuer)
+    logger.write('System message: ' + result.message)
+    if (response.ok) {
+      logger.write('UserID: ' + result.user_id)
+    }
 
+    if (result.event_log) {
+      logger.write('Event log: ...................\n')
+      result.event_log.forEach(element => {
+        logger.write(element + '\n')
+      })
+      logger.write('....................')
+    }
+
+    logger.line()
     console.log(result)
   } catch (error) {
     console.log(error)
   }
 }
 
-let button = document.querySelector('#login_button')
-
-button.addEventListener('click', (event) => {
+let loginButton = document.querySelector('#login_button')
+loginButton.addEventListener('click', (event) => {
   let email = document.querySelector('input[name="email"]').value
   let password = document.querySelector('input[name="password"]').value
 
   login(loginURL, email, password)
+})
+
+let clearButton = document.querySelector('#clear_button')
+clearButton.addEventListener('click', (event) => {
+  logger.clear()
 })
