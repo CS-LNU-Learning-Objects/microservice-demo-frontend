dd569d293a8d373147c5c9b4abfcaf77addeb59c
John Häggerud <john.haggerud@lnu.se>
2019-05-17 16:40:30 +0200

    adding logger, updating code

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 import { Logger } from './Logger.js'
 
 const loginURL = 'http://localhost:5000/authenticate'
+const validateTokenURL = 'http://localhost:5000/token/validate'
+
 let logger = new Logger()
 logger.write('Init the logger...')
+
+let token = ''
 
 let login = async (url, email, password) => {
   logger.write('user try to login....')
@@ -17,19 +21,58 @@ let login = async (url, email, password) => {
       },
       body: JSON.stringify({ email: email, password: password })
     })
-    console.log(response)
+
     logger.write('HTTP status: ' + response.status)
     let result = await response.json()
     logger.write('Issuer: ' + result.issuer)
     logger.write('System message: ' + result.message)
     if (response.ok) {
       logger.write('UserID: ' + result.user_id)
+      token = result.token
+      logger.write('Token: ' + result.token)
     }
 
     if (result.event_log) {
-      logger.write('Event log: ...................\n')
+      logger.write('Event log: ...................')
       result.event_log.forEach(element => {
-        logger.write(element + '\n')
+        logger.write(element)
+      })
+      logger.write('....................')
+    }
+
+    logger.line()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+let validateToken = async (url, token) => {
+  logger.write('Starting to validate the token')
+  logger.line()
+  logger.write('Trying to validate: ' + token)
+
+  try {
+    let response = await window.fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': token
+      }
+    })
+    // console.log(response)
+    logger.write('HTTP status: ' + response.status)
+    let result = await response.json()
+    logger.write('Issuer: ' + result.issuer)
+    logger.write('System message: ' + result.message)
+    if (response.ok) {
+      logger.write('UserID: ' + result.user_id)
+      token = result.user_id
+    }
+
+    if (result.event_log) {
+      logger.write('Event log: ...................')
+      result.event_log.forEach(element => {
+        logger.write(element)
       })
       logger.write('....................')
     }
@@ -47,6 +90,11 @@ loginButton.addEventListener('click', (event) => {
   let password = document.querySelector('input[name="password"]').value
 
   login(loginURL, email, password)
+})
+
+let validateTokenButton = document.querySelector('#token_button')
+validateTokenButton.addEventListener('click', (event) => {
+  validateToken(validateTokenURL, token)
 })
 
 let clearButton = document.querySelector('#clear_button')
